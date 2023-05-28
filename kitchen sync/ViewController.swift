f4dbbcb41ec2a1e4160ce9084f65ee9391f76cd2
Pelle Fredrikson <pelle.fredrikson@academedia.se>
2023-05-28 17:46:56 +0200

    Initial Commit

---
+++ kitchen sync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kitchen sync
+//
+//  Created by Pelle Fredrikson on 2023-05-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
